f2eb8333b55e72f46a04d047497987d23a25806f
Elizabeth J <elizabeth@wondertwinsventures.com>
2018-08-04 10:05:01 -0400

    created migration to create teacher and kid tables

--- db/migrate/20180804020027_create_kids.rb
+++ db/migrate/20180804020027_create_kids.rb
@@ -1,4 +1,10 @@
 class CreateKids < ActiveRecord::Migration
+  
   def change
+
+  	create_table :kids do |k|
+  		k.integer :kid_id
+  		k.string :name
+  		
   end
 end
--- db/migrate/20180804020057_create_teachers.rb
+++ db/migrate/20180804020057_create_teachers.rb
@@ -1,4 +1,12 @@
 class CreateTeachers < ActiveRecord::Migration
+  
   def change
+
+  	create_table teachers do |t|
+  		t.integer :teacher_id
+  		t.string :name
+  	end
+
   end
+
 end
