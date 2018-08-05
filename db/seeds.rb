c6f3727f4f996a5412d7ecb1a697678c0e65c5c3
Elizabeth J <elizabeth@wondertwinsventures.com>
2018-08-05 12:18:36 -0400

    added seed data (teachers and students) for the database

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+puts "Creating Teachers..."
+Teacher.create(name: "Mary Lou")
+Teacher.create(name: "Sue Johnson")
+
+
+puts "Creating Kids..."
+Kids.create(name: "Little Jonny")
+Kids.create(name: "Sally Sue")
+Kids.create(name: "Matilda Moye")
+Kids.create(name: "Bob Hailey")
