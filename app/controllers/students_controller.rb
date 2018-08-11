2db61a50f80a3b9aba68739c708adf16f94b9721
Elizabeth J <elizabeth@wondertwinsventures.com>
2018-08-10 20:23:57 -0400

    add logic setting session to teacher.id if teacher is found

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,21 +5,27 @@ class StudentsController < ApplicationController
       set :session_secret, "secret"
     end
 
-    get "/start" do
+    get "/" do
     	erb :"/index"
 	end	
+
 	
 	get "/students/:id" do
 		
 		erb :"/show"
 	end
 
-	post "/students" do
+	post "/login" do
 
 		@teacher = Teacher.find(param[:teacher])
 		#set @teacher.id to session.id ??
 
-		redirect :"/students/#{@teacher.id}"
+		if @teacher
+      		session[:user_id] = @teacher.id
+      		redirect "/roster"
+    	else
+      		erb :error
+    	end
 	end
 
 	
